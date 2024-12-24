d21821ed944f027616c05c3c2e7e4b6c9b3f587c
DESKTOP-52351T8\Admin <ee2ssam@gmail.com>
2024-12-24 13:08:13 +0900

    1. ServicesController.cs 예제

--- ApiHelloWorld/obj/Debug/net8.0/ApiHelloWorld.AssemblyInfo.cs
+++ ApiHelloWorld/obj/Debug/net8.0/ApiHelloWorld.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiHelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feabbec92f73b2d610a071341b4a01db2eb87117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56cb0b8871d5cbc52b8a4ec275ff8a8aa20c9e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiHelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiHelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
